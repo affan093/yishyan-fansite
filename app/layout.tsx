98a2826e16ed1f8a6f7a6bebb378c411d4f007f4
affan093 <affand.actuary93@gmail.com>
2026-06-26 09:33:54 +0800

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,6 +21,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
+      
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CVZN16CDDY"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CVZN16CDDY');
+          `}
+        </Script>
+      </head>
+      
       <body className="min-h-full font-sans pb-24 md:pb-28">{children}</body>
     </html>
   );
